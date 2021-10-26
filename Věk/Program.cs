39ea829ed7ff971a9336a505f42b2b704838e9a8
petru.stepan <petru.stepan@D20-05>
2021-10-26 11:36:56 +0200

    Udelal

--- Věk/Program.cs
+++ Věk/Program.cs
@@ -6,7 +6,30 @@ namespace Věk
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double rok_narozeni;
+            double vek;
+
+            Console.CursorVisible = false;
+            Console.SetCursorPosition (10, 10);
+            Console.WriteLine();
+            Console.SetCursorPosition(10, 11);
+            Console.WriteLine(" Kolik ti je let?");
+            double.TryParse(Console.ReadLine(), out rok_narozeni);
+            Console.SetCursorPosition(10, 12);
+            Console.WriteLine();
+            Console.ReadLine();
+
+                
+
+        }
+
+
+
+
+
+      
+
+
         }
     }
-}
+
