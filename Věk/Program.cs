cb82c3a0838e836c79b0816fad33d270f027ab53
petru.stepan <petru.stepan@D20-05>
2021-10-26 10:27:27 +0200

    Add project files.

---
+++ Věk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Věk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
